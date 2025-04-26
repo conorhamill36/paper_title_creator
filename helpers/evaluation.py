d47ee00a59686a589f1fa532ee529f9b802026c2
Conor Hamill <conorhamill36@hotmail.com>
2025-04-26 18:41:30 +0100

    lint with black

--- helpers/evaluation.py
+++ helpers/evaluation.py
@@ -1,6 +1,7 @@
 """
 Helper functions for model evaluation
 """
+
 from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
@@ -8,31 +9,36 @@ import pandas as pd
 TEST_SET_PATH: str = "test_set/"
 TEST_SET_DATAFRAME_FILENAME: str = "test_set.csv"
 TEST_SET_FILENAMES: list[str] = [
-    '1409.0575.pdf',
- '1512.03385.pdf',
- '1603.02754.pdf',
- '1706.03762.pdf',
- '1810.04805.pdf',
- '2209.00939.pdf',
- '2311.01901.pdf',
- '2311.05232.pdf',
- '43022.pdf',
- 'Gradient-based_learning_applied_to_document_recognition.pdf']
+    "1409.0575.pdf",
+    "1512.03385.pdf",
+    "1603.02754.pdf",
+    "1706.03762.pdf",
+    "1810.04805.pdf",
+    "2209.00939.pdf",
+    "2311.01901.pdf",
+    "2311.05232.pdf",
+    "43022.pdf",
+    "Gradient-based_learning_applied_to_document_recognition.pdf",
+]
 
 
-def evaluate_model(preds: np.array, df_test: pd.DataFrame, filenames: list[str] = None, years_col: str = "year", name_col:str="name") -> dict[str, float]:
+def evaluate_model(
+    preds: np.array,
+    df_test: pd.DataFrame,
+    filenames: list[str] = None,
+    years_col: str = "year",
+    name_col: str = "name",
+) -> dict[str, float]:
     if filenames is None:
-        filenames=TEST_SET_FILENAMES
-    
+        filenames = TEST_SET_FILENAMES
+
     # parsing years and names from preds
     years = [int(pred.split("_")[0]) for pred in preds]
     names = [pred.split("_")[1] for pred in preds]
-    
 
     # making evaluation dictionary for outputting
     eval_dict = {}
     eval_dict["years_accuracy"] = accuracy_score(years, df_test[years_col].values)
     eval_dict["names_accuracy"] = accuracy_score(names, df_test[name_col].values)
-    
+
     return eval_dict
-    
